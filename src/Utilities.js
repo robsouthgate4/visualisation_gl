89912371124da42db70d38d96d28119750763002
robsouthgate4 <robsouthgate4@gmail.com>
2020-01-11 13:02:47 +0000

    Added prev velocity

---
+++ src/Utilities.js
@@ -0,0 +1,10 @@
+export default class Utilities {
+
+    static createDataTexture ( data ) {
+
+        return new Uint8Array( data.length );
+
+    }
+
+
+}
--- src/shaders/velocityFrag.glsl
+++ src/shaders/velocityFrag.glsl
@@ -3,6 +3,8 @@
 uniform float time;
 uniform float delta;
 
+uniform sampler2D uTextureVelocity;
+
 const float PI = 3.141592653589793;
 const float PI_2 = PI * 2.0;
 
@@ -10,9 +12,11 @@ varying vec2 vUv;
 
 void main() {    
 
-
     vec3 acceleration = vec3( 0. );
-    vec3 velocity = vec3( 0.001 );
 
-    gl_FragColor = vec4(velocity, 1.0);
+    vec3 prevVelocity = texture2D( uTextureVelocity, vUv ).xyz;
+
+    vec3 velocity = prevVelocity += 0.000001;
+ 
+    gl_FragColor = vec4( velocity, 1.0 );
 }
