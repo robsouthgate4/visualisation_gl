03bd2eb7283b047a0ff6737b98556bee54f4ad66
robsouthgate4 <robsouthgate4@gmail.com>
2020-02-28 12:01:35 +0000

    And were there :)

--- src/PostProcess/shaders/motionFragment.glsl
+++ src/PostProcess/shaders/motionFragment.glsl
@@ -16,7 +16,7 @@ void main( void ) {
 
     vel = ( vPosition.xyz / vPosition.w ) - ( vPrevPosition.xyz / vPrevPosition.w );
 
-    vel *= 10.0;
+    vel *= 5.0;
 
     gl_FragColor = vec4( vel, 1.0);
 
